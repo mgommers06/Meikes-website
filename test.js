9e2033a6cd7bc73b456a1006f16280a2f6e092fa
mgommers06 <m.gommers@tilburguniversity.edu>
2026-09-01 13:59:36 +0200

    Initial commit

---
+++ test.js
@@ -0,0 +1,21 @@
+// console.log("Hello");
+// console.log("I like pizza")
+
+// window.alert(`This is an alert!`);
+// window.alert(`I like pizza!`);
+
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `I like pizza`
+//comment
+// let username;
+// document.getElementById("mySubmit").onclick = function(){
+//     username = document.getElementById("myText").value;
+//     document.getElementById("myH1").textContent = `Hello ${username}`
+//     // console.log(username);
+// } 
+
+const reset= 0
+document.getElementById("reset").onclick = function(){
+    document.getElementById().textcontent= reset
+
+}
